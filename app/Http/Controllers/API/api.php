3421775714dbb0646481acba6df4a72cc79e6f13
Abdullah Hussein <abdullabale11@gmail.com>
2023-11-13 11:55:26 +0300

    add api controller

---
+++ app/Http/Controllers/API/api.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class api extends Controller
+{
+    public function home(){
+        return 'somethin';
+    }
+}
